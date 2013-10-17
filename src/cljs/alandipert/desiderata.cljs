5f74d672fbd45a6bf61cdd56efefefae529f3ec5
Micha Niskin <asdf@ubergibson.com>
2013-10-17 15:08:19 -0400

    change static field access to new cljs format

--- src/cljs/alandipert/desiderata.cljs
+++ src/cljs/alandipert/desiderata.cljs
@@ -8,4 +8,4 @@
               (lazy-seq
                (cons node (walk (into (pop queue)
                                       (if (branch? node) (children node))))))))]
-    (walk (conj cljs.core.PersistentQueue/EMPTY root))))
+    (walk (conj cljs.core.PersistentQueue.EMPTY root))))
